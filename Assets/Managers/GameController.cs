4e4c0c14515973add53b161b7594f791c02ff453
Arno Poppe <minoarno@gmail.com>
2026-08-21 21:54:09 +0200

    Hourglass flipping + flipping behavior + button

--- Assets/Managers/GameController.cs
+++ Assets/Managers/GameController.cs
@@ -1,7 +1,28 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
+    public int Age => _age;
+    private int _age = 0;
+
+    private List<FlipBehaviour> _flippers = new();
+
+    public static GameController Instance;
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,5 +33,25 @@ public class GameController : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void Flip()
+    {
+        for(int i = 0; i < _flippers.Count; i++)
+        {
+            _flippers[i].Flip();
+        }
+        _age++;
+    }
+
+    public void SubscribeFlipper(FlipBehaviour flipper)
+    {
+        _flippers.Add(flipper);
+    }
+
+
+    public void UnsubscribeFlipper(FlipBehaviour flipper)
+    {
+        _flippers.Remove(flipper);
     }
 }
--- Assets/UI/FlipBehaviour.cs
+++ Assets/UI/FlipBehaviour.cs
@@ -1,16 +1,34 @@
+using System;
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class FlipBehaviour : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    float _desiredRotation = 0f;
+    [SerializeField] private float _rotationSpeed;
+
+    private void Start()
     {
-        
+        GameController.Instance.SubscribeFlipper(this);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDestroy()
     {
-        
+        GameController.Instance.UnsubscribeFlipper(this);
+    }
+
+    private void Update()
+    {
+        float currentAngle = transform.eulerAngles.z;
+        float difference = (_desiredRotation - currentAngle) % 360;
+        if (difference < 0f) difference += 360;
+
+        transform.rotation = Quaternion.Euler(0, 0, currentAngle + difference * _rotationSpeed * Time.deltaTime);
+    }
+
+    public void Flip()
+    {
+        _desiredRotation += 180;
     }
 }
