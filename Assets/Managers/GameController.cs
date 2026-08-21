b9c11fddc1bb306ed7467cc4a7e1d567f56e88d7
Arno Poppe <minoarno@gmail.com>
2026-08-21 23:27:28 +0200

    transparent sand

--- Assets/Managers/GameController.cs
+++ Assets/Managers/GameController.cs
@@ -8,6 +8,15 @@ public class GameController : MonoBehaviour
     public int Age => _age;
     private int _age = 0;
 
+    public int Sand => _sand;
+    private int _sand = 0;
+    [SerializeField] private int _initialSand = 500;
+
+    [SerializeField] private int _sandlossPerTurn = 100;
+
+    private float _lastTimeCheck = 0;
+    [SerializeField] private float _timeCheckDelay = 1f;
+
     private List<FlipBehaviour> _flippers = new();
 
     public static GameController Instance;
@@ -26,13 +35,21 @@ public class GameController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        _sand = _initialSand;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (_lastTimeCheck + _timeCheckDelay > Time.time) return;
+        _lastTimeCheck = Time.time;
+
+        _sand--;
+
+        if( _sand < 0 )
+        {
+            Lose();
+        }
     }
 
     public void Flip()
@@ -42,6 +59,7 @@ public class GameController : MonoBehaviour
             _flippers[i].Flip();
         }
         _age++;
+        _sand -= _sandlossPerTurn;
     }
 
     public void SubscribeFlipper(FlipBehaviour flipper)
@@ -53,5 +71,10 @@ public class GameController : MonoBehaviour
     public void UnsubscribeFlipper(FlipBehaviour flipper)
     {
         _flippers.Remove(flipper);
+    }
+
+    private void Lose()
+    {
+        
     }
 }
--- Assets/UI/UIManager.cs
+++ Assets/UI/UIManager.cs
@@ -2,8 +2,25 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
+    public TMPro.TMP_Text sandText;
+
     public void Flip()
     {
         GameController.Instance.Flip();
+    }
+
+    private void Start()
+    {
+        UpdateSand();
+    }
+
+    private void LateUpdate()
+    {
+        UpdateSand();
+    }
+
+    public void UpdateSand()
+    {
+        sandText.text = "Sand: " + GameController.Instance.Sand;
     }
 }
